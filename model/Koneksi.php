9bd03dda23d53e49dc234f6d397208852fd42f07
asyrafdhlrrhmn <asyrafdhlrrhmn@gmail.com>
2025-12-11 23:44:21 +0700

    env

--- model/Koneksi.php
+++ model/Koneksi.php
@@ -1,17 +1,26 @@
 <?php
 class koneksi {
-    private $host = "localhost";
-    private $user = "root";
-    private $password = "";
-    private $dbname = "cloudify";
+
+    private $host;
+    private $user;
+    private $password;
+    private $dbname;
 
     public $conn;
 
     public function __construct() {
+
+        $this->host     = getenv('DB_HOST');
+        $this->user     = getenv('DB_USER');
+        $this->password = getenv('DB_PASS');
+        $this->dbname   = getenv('DB_NAME');
+
         $this->conn = new mysqli($this->host, $this->user, $this->password, $this->dbname);
+
         if ($this->conn->connect_error) {
             die('Koneksi gagal: ' . $this->conn->connect_error);
         }
+
         $this->conn->set_charset('utf8mb4');
     }
 
